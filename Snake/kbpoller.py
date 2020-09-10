a0a101bf9683975fca9253f6d595081b7d05fbe5
Dan Johnson <68550997+djamson@users.noreply.github.com>
2020-09-10 23:55:17 +0200

    Update kbpoller.py

--- Snake/kbpoller.py
+++ Snake/kbpoller.py
@@ -1,3 +1,24 @@
+#!/usr/bin/env python
+# found here:
+# http://stackoverflow.com/questions/2408560/python-nonblocking-console-input
+# tested by JCF on Linux
+'''
+A Python class implementing KBHIT, the standard keyboard-interrupt poller.
+Works transparently on Windows and Posix (Linux, Mac OS X).  Doesn't work
+with IDLE.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as 
+published by the Free Software Foundation, either version 3 of the 
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+'''
+
 import os
 import queue
 
