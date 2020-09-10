ff165f0005f805d5dfbfc8f1f0a260cc372e8d0d
Dan Johnson <68550997+djamson@users.noreply.github.com>
2020-09-10 23:50:00 +0200

    Update game.py

--- Snake/game.py
+++ Snake/game.py
@@ -1,15 +1,16 @@
-
-# TODO
-# COMPLETE - Food to not spawn on top of snake
-# COMPLETE - Handler for turning 180 degrees 
-# COMPLETE - Trigger eat food as tick enters position instead of on leave
-# - Create tick rate progression for difficulty increase
-# COMPLETE - Snake to die when boundary hit
-# - Display controls down side of scene
-# - Display score on death
-# - Create menu
-# - Snake to spawn randomly with safe padding
-# COMPLETE - Add full notes
+'''
+TODO
+COMPLETE - Food to not spawn on top of snake
+COMPLETE - Handler for turning 180 degrees 
+COMPLETE - Trigger eat food as tick enters position instead of on leave
+- Create tick rate progression for difficulty increase
+COMPLETE - Snake to die when boundary hit
+- Display controls down side of scene
+- Display score on death
+- Create menu
+- Snake to spawn randomly with safe padding
+COMPLETE - Add full notes
+'''
 
 import elements as el
 import kbpoller as kb
@@ -17,50 +18,53 @@ import os, time, queue, threading
 from platform import system
 
 class Game:
+
     def __init__(self, height, width, refresh_rate, difficulty):
         self.height = height
         self.width = width
         self.refresh_rate = refresh_rate
         self.snake = el.Snake([(3,1),(2,1),(1,1)],el.Snake.DOWN)
-        self.food = el.Food("*", (5, 5))
+        self.food = el.Food((5, 5))
         self.score = 0
         self.difficulty = difficulty
 
+
     def run(self):
-        # Start listener on to seperate thread to avoid blockin
-        # Trigger order: user, tick, render, sleep * refer_rate
+
+        # Start listener new thread to avoid blocking
         threading.Thread(target=kb.listen, daemon=True).start()
+
+        # Game loop
         while True:
             tic = time.perf_counter()
             self.on_user()
             self.on_tick()
-            self.set_scene()
+            self.set_matrix()
             self.render()
             toc = time.perf_counter()
             tElapsed = toc - tic
             time.sleep(self.refresh_rate - tElapsed)
 
+
     def on_tick(self):
-        # Trigger on every tick @ refresh_rate
-        # Move snake to new position
+
+        self.snake.take_step(self.height, self.width)   
+
         # Check if new position contains food and eat
-        # Check is snake had died
-        clear_console()
-        self.snake.take_step(self.height, self.width)      
         if self.snake.body[0] == self.food.position:
+
             self.snake.body.insert(0, self.food.position)
-            self.food.get_position(self.height, 
-                self.width,self.snake.body)
+            self.food.get_position(self.height, self.width,self.snake.body)
             self.score += 1
+
         elif self.snake.life == 0:
+
             exit()
-        else:
-            pass    
+
 
     def on_user(self):
-        # Trigger on user input after refresh
-        # Check key_press and assign to controls
 
+        # Set controls
         if str(kb.key) == 'w':
             self.snake.set_direction(el.Snake.UP)
         elif str(kb.key) == 'a':
@@ -69,11 +73,11 @@ class Game:
             self.snake.set_direction(el.Snake.DOWN)
         elif str(kb.key) == 'd':
             self.snake.set_direction(el.Snake.RIGHT)
-        else:
-            pass
 
-    def set_scene(self):
-        # create game area array 
+
+    def set_matrix(self):
+
+        # Create game matrix
         self.matrix = [
             [
                 "-" if y == 0 or y == self.height - 1
@@ -82,30 +86,36 @@ class Game:
                 for x 
                 in range(self.width)
             ]  
+            
             for y 
             in range(self.height)
         ]
-        # Insert snake 
-        bIdx = 0
-        for _ in self.snake.body:
-            self.matrix[self.snake.body[bIdx][0]][self.snake.body[bIdx][1]] = "#"
-            bIdx += 1
-        # Insert food 
-        self.matrix[self.food.position[0]][self.food.position[1]] = self.food.char
 
-        return self.matrix
+        # Insert snake 
+        i = 0
+        for _ in self.snake.body:
+            self.matrix[self.snake.body[i][0]][self.snake.body[i][1]] = "#"
+            i += 1
+
+        # Insert food
+            self.matrix[self.food.position[0]][self.food.position[1]] = "*"
+
 
     def render(self):
-        #print scene
-        print(f"score: {self.score}")
+
+        print(f"SCORE: {self.score}")
+
+        # Print matrix row by row
         for y in range(self.height):
             print(*self.matrix[y])
     
 
 def clear_console():
+
     # Clear console based on OS
     clear = 'cls' if system().lower()=='windows' else 'clear'
     os.system(clear)
 
-
+clear_console()
 Game(20, 30, 0.2, "normal").run()
+
